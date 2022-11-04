6b1da1af082c67821dceca9c80ffda7fb34bf10f
wj <2827765448@qq.com>
2022-11-04 16:08:40 +0800

    first commit

---
+++ src/main/java/com/gdut/imis/maven/Calcultor.java
@@ -0,0 +1,7 @@
+package com.gdut.imis.maven;
+
+public class Calcultor {
+    public int add(int i, int j){
+        return i+j;
+    }
+}
---
+++ src/test/java/com/gdut/imis/maven/CalcultorTest.java
@@ -0,0 +1,16 @@
+package com.gdut.imis.maven;
+
+import org.junit.Test;
+
+public class CalcultorTest {
+    @Test
+    public void testCal() throws Exception {
+        Calcultor calcultor = new Calcultor();
+        int i = calcultor.add(3,5);
+        int j = 8;
+        if(i != j){
+            throw new Exception();
+        }
+        System.out.println("hello");
+    }
+}
